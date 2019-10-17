c3364c09691e85bebe2bdefc68dde6a57cab9cc5
Prashant-JT <55165517+Prashant-JT@users.noreply.github.com>
2019-10-17 22:03:54 +0100

    Delete pkg_randomvector.ads

--- FormalVerificationAda-master/SPARK_Lab/src/pkg_randomvector.ads
+++
@@ -1,7 +0,0 @@
-package Pkg_RandomVector with SPARK_Mode is
-
-   type T_Vector is array (Natural range <>) of Integer;
-   
-   function RandomVector (In1 : Positive; In2 : Positive) return T_Vector
-
-end Pkg_RandomVector;
